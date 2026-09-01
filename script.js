1540c020b7fea95696e46fbf7d08048f0f2601aa
Boivana <boivanaofficial@gmail.com>
2026-09-01 12:05:40 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+// =========================
+// Mobile Menu
+// =========================
+
+const menuBtn = document.getElementById("menuBtn");
+const navLinks = document.getElementById("navLinks");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+
+// Close mobile menu after clicking a link
+
+const links = document.querySelectorAll(".nav-links a");
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
+});
+
+
+// =========================
+// Contact Form
+// =========================
+
+const contactForm = document.getElementById("contactForm");
+
+contactForm.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+
+    alert(
+        `Thank you ${name}! Your message has been received.`
+    );
+
+    contactForm.reset();
+
+});
